df199b32b437765b12a175e0fc97b171dfbf8261
Nicat Ismayilov <n1ko.net.dev@gmail.com>
2022-06-09 22:56:06 +0400

    terms and conditions (fix)

--- pages/terms-and-conditions.vue
+++ pages/terms-and-conditions.vue
@@ -4,24 +4,13 @@
       <div class="response-message about-title">
         <div class="mkub mkubsuccess kubflex"></div>
         <div class="rminfo">
-          <h2 class="rmtt">
-            Terms and Conditions
-          </h2>
+          <h1 class="rmtt">
+            {{ $t('terms_conditions') }}
+          </h1>
         </div>
       </div>
 
-      <div class="terms_txt">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ab ea, dolore laboriosam consequuntur inventore eveniet neque qui facilis officia beatae placeat atque recusandae veritatis nostrum consequatur quidem amet suscipit!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aliquam, quibusdam eum animi, dolor delectus similique debitis voluptates possimus accusantium tempore rem sunt maiores officia voluptatem velit iste ea maxime?
-        <br /> <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores officia quia et maxime sunt, ducimus provident a quasi sit qui ipsum sint fugiat inventore exercitationem earum? Illum modi reprehenderit ut.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, molestiae, voluptates vel doloribus laudantium assumenda aliquam voluptas cum nihil eaque alias iste esse dignissimos natus non adipisci iure officia quibusdam.
-        <br /> <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime numquam ea quae nisi rerum incidunt dolore nesciunt suscipit aperiam voluptate aut distinctio reprehenderit, laboriosam adipisci. Quaerat distinctio sunt animi dolores.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, earum. Ullam voluptates ipsa labore vitae sint reiciendis laboriosam! Fugit ducimus facere ipsum id laudantium optio, doloremque quibusdam aliquam harum quasi?
-        <br /> <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime numquam ea quae nisi rerum incidunt dolore nesciunt suscipit aperiam voluptate aut distinctio reprehenderit, laboriosam adipisci. Quaerat distinctio sunt animi dolores.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, earum. Ullam voluptates ipsa labore vitae sint reiciendis laboriosam! Fugit ducimus facere ipsum id laudantium optio, doloremque quibusdam aliquam harum quasi?
+      <div class="terms_txt" v-html="$t('terms_conditions_text')">
       </div>
     </div>
   </div>
